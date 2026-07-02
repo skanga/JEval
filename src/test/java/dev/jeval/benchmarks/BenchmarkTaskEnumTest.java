f3cdd26ea511a25e9df82c23579dc8c5396864cd
skanga <skanga@gmail.com>
2026-07-01 18:05:29 -0700

    feat: add bigbenchhard task enum

--- src/test/java/dev/jeval/benchmarks/BenchmarkTaskEnumTest.java
+++ src/test/java/dev/jeval/benchmarks/BenchmarkTaskEnumTest.java
@@ -60,6 +60,39 @@ class BenchmarkTaskEnumTest {
     }
 
     @Test
+    void bigBenchHardTaskValuesMatchDeepEval() {
+        assertEquals(List.of(
+                "boolean_expressions",
+                "causal_judgement",
+                "date_understanding",
+                "disambiguation_qa",
+                "dyck_languages",
+                "formal_fallacies",
+                "geometric_shapes",
+                "hyperbaton",
+                "logical_deduction_five_objects",
+                "logical_deduction_seven_objects",
+                "logical_deduction_three_objects",
+                "movie_recommendation",
+                "multistep_arithmetic_two",
+                "navigate",
+                "object_counting",
+                "penguins_in_a_table",
+                "reasoning_about_colored_objects",
+                "ruin_names",
+                "salient_translation_error_detection",
+                "snarks",
+                "sports_understanding",
+                "temporal_sequences",
+                "tracking_shuffled_objects_five_objects",
+                "tracking_shuffled_objects_seven_objects",
+                "tracking_shuffled_objects_three_objects",
+                "web_of_lies",
+                "word_sorting"),
+                Arrays.stream(BigBenchHardTask.values()).map(BigBenchHardTask::value).toList());
+    }
+
+    @Test
     void truthfulQaTaskValuesMatchDeepEval() {
         assertEquals(List.of(
                 "Language",
